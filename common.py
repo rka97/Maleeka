a78356bee05cb69d63bc82e010a1973e69c67b90
Ahmed Khaled <akregeb@gmail.com>
2019-12-23 16:44:07 +0200

    segm works but is shitty

--- common.py
+++ common.py
@@ -4,6 +4,18 @@ from skimage.transform import rotate, resize, rescale
 import deskew
 
 LINE_HEIGHT = 40
+BASELINE_THICKNESS = 2
+BASELINE_SCAFFOLD = 5
+
+
+def highlight(img_like, places, axis=0):
+    img = np.zeros_like(img_like)
+    if axis==0:
+        img[places, :] = 1
+    else:
+        img[:, places] = 1
+    return img
+
 
 def show_images(images):
     for img in images:
@@ -38,6 +50,25 @@ def image_autocrop(img):
     img = img[start:end+1, :]
     return img
 
+def get_bounds(img):
+    # TODO: refactor this.
+    vertical_projection = np.sum(img, axis=0)
+    height, width = img.shape
+    start = 0
+    while vertical_projection[start] == 0:
+        start += 1
+    end = width-1
+    while vertical_projection[end] == 0:
+        end -= 1
+
+    horizontal_projection = np.sum(img, axis=1)
+    start_h = 0
+    while horizontal_projection[start_h] == 0:
+        start_h += 1
+    end_h = height-1
+    while horizontal_projection[end_h] == 0:
+        end_h -= 1
+    return start_h, end_h, start, end
 
 
 def debug_draw(red, blue=None, green=None):
